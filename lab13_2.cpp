63cb0d0add17457b1959b931fff6d84b13c3ce7c
Fluke <hunterx52110@gmail.com>
2021-12-24 16:08:30 +0700

    completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int ,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int num1,int num2){
+    int y;
+    if(num1%num2 == 0){
+        y = num2;
+    }else{
+        y = gcd(num2,num1%num2);
+    }
+    return y;
 }
